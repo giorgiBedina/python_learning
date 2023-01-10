a0009ef58f4cab30b636597c454d4928b77b886f
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-10 23:35:26 +0400

    types of variables in python

---
+++ basics/variables.py
@@ -0,0 +1,20 @@
+# Viriable types int, float, boolean, string
+# int - whole numbers 
+# float - decimal numbers
+# boolean - true/false
+# string - character or array of characters
+
+var_int = 5
+var_float = 4.20
+var_bool = True
+var_string = "This is string"
+
+# isinstance() function checks data type
+
+if isinstance(var_int,int):
+    print("var_int is whole number(int)")
+
+if isinstance(var_float,int):
+    print("var_float is whole number(int)")  
+else:
+    print("var_float is not whole number(int)")  
