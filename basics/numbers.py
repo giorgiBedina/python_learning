7017168aec682ae5eb7267134a72615311e8c197
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-11 13:41:03 +0400

    float  numbers and e

--- basics/numbers.py
+++ basics/numbers.py
@@ -8,3 +8,16 @@ x = 154
 y = -451    
 z = 0
 
+print(x,y,z)
+
+# float - any decimal number
+x = 1.25    
+y = -52.3456
+
+print(x,y)
+
+# float can also be scientific numbers with an "e" to indicate the power of 10.
+numb1 = 3.565e10
+numb2 = -45.1e5
+
+print(numb1,numb2)
