dd466fdfc4087edc96020eb01a42b6e856904f2a
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-11 14:38:44 +0400

    print strings

---
+++ basics/strings.py
@@ -0,0 +1,6 @@
+# Strings is array of characters 
+# pring string 
+print("this is string")
+# you can print space and end-line
+print("Hello\tworld") # \t prints space
+print("first line\nsecond line") # \n prints endline
