4270d95122c7495c80c20a6a78b587196a98ba42
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-11 14:50:03 +0400

    multiline strings

--- basics/strings.py
+++ basics/strings.py
@@ -4,3 +4,18 @@ print("this is string")
 # you can print space and end-line
 print("Hello\tworld") # \t prints space
 print("first line\nsecond line") # \n prints endline
+
+# you can assingn string to a variable 
+str1 = "sting one"
+print(str1)
+
+# multiline strings
+m_string = """"Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(m_string)
+
+# strings are arreys , therefore you can separate characters from stings
+str2 = str("Earth is flat")
+print(str2[0]) # this will print first character of the string
