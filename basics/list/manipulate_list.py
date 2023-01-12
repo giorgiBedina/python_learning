0817b5b65758f45fb47f112508cbd220cd409304
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-12 16:16:53 +0400

    while loop in list

--- basics/list/manipulate_list.py
+++ basics/list/manipulate_list.py
@@ -1,4 +1,5 @@
 # Loop through list
+# for loop
 # You can loop through the list items by using a for loop
 list_OS = ["windows 10","windows 7","arch linux","debian","macOS"]
 for OS in list_OS:
@@ -9,3 +10,13 @@ print()
 list_linux = ["manjaro","ubuntu","mint","kali","fedora"]
 for i in range(len(list_linux)): # Use the range() and len() functions to create a suitable iterable
     print(list_linux[i])
+    
+# while loop
+# You can loop through the list items by using a while loop
+# Use the len() function to determine the length of the list, then start at 0 and 
+# loop your way through the list items by referring to their indexes    
+car_gears = ["reverse gear","first gear","second gear","forth gear","fifth gear"]
+i = 0
+while 1 < len(car_gears):
+    print(car_gears[i],end='\t')
+    i += 1 # Remember to increase the index by 1 after each iteration
