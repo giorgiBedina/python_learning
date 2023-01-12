65d797e6688df00afe91a1c5e0acabeae41c4d7a
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-12 12:22:22 +0400

    define list

---
+++ basics/list.py
@@ -0,0 +1,11 @@
+# Lists are used to store multiple items in a single variable.
+footballers = ["messi","mbappe","neymar"]
+print(footballers)
+
+# List items are ordered, changeable, and allow duplicate values.
+# List items are indexed, the first item has index [0], the second item has index [1] etc.
+print(footballers[0]) # This will print the first item on the list , which is messi
+
+# The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+# Since lists are indexed, lists can have items with the same value
+fruits = {"apple","banana","cherry","banana"}
