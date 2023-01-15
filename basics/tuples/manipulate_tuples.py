df81054f7f859bff2fcfdd35b8cd5b6c6b3a53e4
giorgi <bedinashvili.giorgi@hotmail.com>
2023-01-15 16:33:37 +0400

    unpacking tuple

--- basics/tuples/manipulate_tuples.py
+++ basics/tuples/manipulate_tuples.py
@@ -50,3 +50,36 @@ print(this_tuple)
 
 # The del keyword can delete the tuple completely
 del this_tuple
+
+# Unpacking a Tuple
+# When we create a tuple, we normally assign values to it. This is called "packing" a tuple
+car_tuple = ("Civic Type R","AMG GTR","GTR Nissmo","GR Supara","M3")
+
+#But, in Python, we are also allowed to extract the values back into variables. This is called "unpacking"
+(honda,mercedes,nissan,toyota,bmw) = car_tuple
+
+print("honda ",honda)
+print("mercedes ",mercedes)
+print("nissan ",nissan)
+print("toyota ",toyota)
+print("bmw ", bmw)
+
+# If the number of variables is less than the number of values,
+# you can add an * to the variable name and the values will be assigned to the variable as a list
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# If the asterisk is added to another variable name than the last, Python will assign values to
+# the variable until the number of values left matches the number of variables left
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
